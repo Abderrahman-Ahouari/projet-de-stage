d2cbc345e5be6fc6625e2e49a6c122b2e1da85f1
yassine snaiki <yassnaiki@gmail.com>
2025-03-17 14:23:11 +0000

    project crud

---
+++ api/app/Http/Controllers/ProjectController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::paginate(10);
+        if ($projects->isEmpty()) {
+            return response()->json([
+                'message' => 'No projects found'
+            ], 404);
+        }
+        return response()->json($projects);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:20',
+            'description' => 'string|max:255',
+        ]);
+        $project = Project::create($data);
+        return response()->json(['project' => $project], 201);
+    }
+
+    public function show(Project $project)
+    {
+        return response()->json($project);
+    }
+
+
+    public function update(Request $request, Project $project)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $project->update($validated);
+
+        return response()->json(['message' => 'Project updated successfully', 'project' => $project], 200);
+    }
+
+
+    public function patch(Request $request, Project $project)
+    {
+        $validated = $request->validate([
+            'title' => 'sometimes|string|max:255',
+            'description' => 'sometimes|nullable|string',
+        ]);
+
+        $project->update($validated);
+
+        return response()->json(['message' => 'Project updated successfully', 'project' => $project], 200);
+    }
+
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return response()->json(['message' => 'Project deleted successfully'], 200);
+    }
+
+}
