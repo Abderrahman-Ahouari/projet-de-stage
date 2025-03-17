0ffc08b1fc983cb8edcb7d4ead4c2fc4c6a7184f
yassine snaiki <yassnaiki@gmail.com>
2025-03-17 14:46:26 +0000

    task factory

---
+++ api/database/factories/TaskFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => Str::uuid(), // Generate a random UUID
+            'title' => $this->faker->sentence, // Random title
+            'description' => $this->faker->paragraph, // Random description
+            'project_id' => Project::factory(), // Assuming you have a Project model factory
+            'status' => $this->faker->randomElement(['todo', 'doing', 'review', 'done']), // Random status
+            'category_id' => Category::factory(),
+        ];
+    }
+}
