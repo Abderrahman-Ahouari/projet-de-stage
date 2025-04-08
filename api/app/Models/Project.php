478da4367818edc8ad8d1d0ec8777f88a443b740
yassine snaiki <yassnaiki@gmail.com>
2025-04-08 14:19:46 +0100

    project relation with contributors and projects

--- api/app/Models/Project.php
+++ api/app/Models/Project.php
@@ -12,6 +12,19 @@ class Project extends Model
     /** @use HasFactory<\Database\Factories\ProjectFactory> */
     use HasFactory,HasApiTokens,HasUuids;
 
+    public function tasks(){
+        return $this->hasMany(Task::class);
+    }
+    public function contributors(){
+        return $this->belongsToMany(User::class,'contributions')
+        ->withPivot('role_id')
+        ->wherePivot('role_id','!=',1);
+    }
+    public function admin(){
+        return $this->belongsToMany(User::class,'contributions')
+        ->withPivot('role_id')
+        ->wherePivot('role_id',1);
+    }
     protected $fillable = [
         'title',
         'description'
