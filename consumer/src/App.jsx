9e05bc8e605603e26777c0e4ee11ce85aca3ae15
yassine snaiki <yassnaiki@gmail.com>
2025-04-08 09:12:51 +0100

    front end routes

--- consumer/src/App.jsx
+++ consumer/src/App.jsx
@@ -6,19 +6,30 @@ import Header from "./components/Header";
 import Login from "./pages/login";
 import Register from "./pages/Register";
 import Kanban from "./pages/Kanban";
+import Projects from "./pages/Projects";
+import Progress from "./pages/Progress";
+import ManageTeam from "./pages/ManageTeam";
+import OAuthSuccess from "./pages/OauthSuccess";
+import AuthProvider from "./contexts/AuthContext";
 
 
 export default function App() {
 
   return (
+    <AuthProvider>
     <Router>
       <Header />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/projects" element={<Projects/>} />
         <Route path="/kanban" element={<Kanban/>} />
+        <Route path="/progress" element={<Progress/>} />
+        <Route path="/manage-team" element={<ManageTeam/>} />
+        <Route path="/oauth-success" element={<OAuthSuccess/>} />
       </Routes>
     </Router>
+    </AuthProvider>
   );
 }
 
