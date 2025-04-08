5945784628429247abf02851a434127a07257b9e
yassine snaiki <yassnaiki@gmail.com>
2025-04-08 14:17:16 +0100

    tasks migration

---
+++ api/database/migrations/2025_03_14_150711_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(DB::raw('uuid_generate_v4()'));
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->uuid('project_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->enum('status', ['todo', 'doing', 'review', 'done'])->default('todo');
+            $table->foreignId('category_id')->nullable()->constrained('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
