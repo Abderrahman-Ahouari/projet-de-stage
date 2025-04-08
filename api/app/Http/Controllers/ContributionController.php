eff0c6b1839bffacdc1d71089f3e742ad407d06c
yassine snaiki <yassnaiki@gmail.com>
2025-04-08 14:21:15 +0100

    contribution controller

---
+++ api/app/Http/Controllers/ContributionController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contribution;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ContributionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Project $project)
+    {
+        return response()->json($project->contributors);
+    }
+
+    public function store(Request $request, Project $project)
+    {
+        $data = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'role_id' => 'required|exists:roles,id',
+        ]);
+        $data['project_id'] = $project->id;
+        $contribution = Contribution::create($data);
+        return response()->json($contribution);
+    }
+
+
+    public function show(Project $project,Contribution $contribution)
+    {
+        return response()->json($contribution);
+    }
+
+
+
+
+    public function update(Request $request,Project $project, Contribution $contribution)
+    {
+        $request->validate([
+            'role_id' => 'required|exists:roles,id',
+        ]);
+        $contribution->update($request->all());
+        return response()->json("Contribution updated successfully");
+    }
+
+    public function destroy(Project $project ,Contribution $contribution)
+    {
+        $contribution->delete();
+        return response()->json("Contribution deleted successfully");
+    }
+}
