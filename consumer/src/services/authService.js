ea08c02b1e6fb738e2d3f5154f42af2549174836
yassine snaiki <yassnaiki@gmail.com>
2025-04-12 19:20:12 +0100

    helpers for axios front end

---
+++ consumer/src/services/authService.js
@@ -0,0 +1,7 @@
+// src/services/authService.js
+import api from "../lib/axios";
+
+export const login = (credentials) => api.post("/login", credentials);
+export const register = (credentials) => api.post("/register", credentials);
+export const logout = () => api.post("/logout");
+export const getUser = () => api.get("/user");
