6bd4763d7e7eea764b99386b4843d8f983223cfa
yassine snaiki <yassnaiki@gmail.com>
2025-04-18 09:40:36 +0100

    importing react query

--- consumer/src/App.jsx
+++ consumer/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -11,7 +10,7 @@ import Progress from "./pages/Progress";
 import ManageTeam from "./pages/ManageTeam";
 import OAuthSuccess from "./pages/OauthSuccess";
 import AuthProvider from "./contexts/AuthContext";
-import ProjectsProvider from "./contexts/ProjectsContext";
+
 
 
 export default function App() {
@@ -19,18 +18,17 @@ export default function App() {
   return (
     <Router>
       <AuthProvider>
-      <ProjectsProvider>
         <Header />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/projects" element={<Projects/>} />
-        <Route path="/kanban" element={<Kanban/>} />
-        <Route path="/progress" element={<Progress/>} />
-        <Route path="/manage-team" element={<ManageTeam/>} />
+        <Route path="/projects/:id/kanban" element={<Kanban/>}/>
+        <Route path="/projects/:id/progress" element={<Progress/>} />
+        <Route path="/projects/:id/team" element={<ManageTeam/>} />
+        <Route path="/projects/:id/settings" element={<ManageTeam/>} />
         <Route path="/oauth-success/:token" element={<OAuthSuccess/>} />
       </Routes>
-      </ProjectsProvider>
     </AuthProvider>
     </Router>
   );
