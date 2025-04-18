df13505dd9442ea5d36c0541627bb0e128a20ed6
yassine snaiki <yassnaiki@gmail.com>
2025-04-18 09:36:35 +0100

    backend updating category controller to depend on project

--- api/app/Http/Controllers/CategoryController.php
+++ api/app/Http/Controllers/CategoryController.php
@@ -3,29 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
 
-    public function index()
+    public function index(Project $project)
     {
-        $category = Category::all();
-        return response()->json(['categories'=>$category]);
+        return response()->json($project->categories);
     }
 
 
-    public function store(Request $request)
+    public function store(Request $request , Project $project)
     {
         $request->validate([
             'name' => 'required|string|max:255',
         ]);
-        $category = Category::create($request->all());
+        $category = $project->categories()->create($request->all());
         return response()->json($category, 201);
     }
 
 
-    public function update(Request $request, Category $category)
+    public function update(Request $request, Category $category, Project $project)
     {
         $request->validate([
             'name' => 'required|string|max:255',
@@ -34,9 +34,9 @@ class CategoryController extends Controller
         return response()->json($category);
     }
 
-    public function destroy(Category $category)
+    public function destroy(Category $category,Project $project)
     {
-        $category->delete();
+        $project->categories()->delete();
         return response()->json(null, 204);
     }
 }
