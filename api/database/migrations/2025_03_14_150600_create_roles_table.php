362619b43820d4cc780163537ad8f9c9851d1ede
yassine snaiki <yassnaiki@gmail.com>
2025-04-18 09:15:27 +0100

    backend linking role to project in migration

--- api/database/migrations/2025_03_14_150600_create_roles_table.php
+++ api/database/migrations/2025_03_14_150600_create_roles_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->uuid('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
             $table->timestamps();
         });
         if (!DB::table('roles')->where('name', 'admin')->exists()) {
