94e433f768dd4287937cd31cffca4a9ad9919c99
yassine snaiki <yassnaiki@gmail.com>
2025-04-18 09:37:50 +0100

    backend updating role controller to depend on project

--- api/app/Http/Controllers/RoleController.php
+++ api/app/Http/Controllers/RoleController.php
@@ -2,45 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
 
-    public function index()
+    public function index(Project $project)
     {
-        $roles = Role::all();
-        return response()->json($roles);
+        return response()->json($project->roles);
     }
 
 
 
-    public function store(Request $request)
+    public function store(Request $request ,Project $project)
     {
         $request->validate([
             'name' => 'required|string|max:255',
         ]);
-        $role = Role::create($request->all());
+        $role = $project->roles()->create($request->all());
         return response()->json($role, 201);
     }
 
 
 
 
-    public function update(Request $request, Role $role)
+    public function update(Request $request, Role $role,Project $project)
     {
         $request->validate([
             'name' => 'required|string|max:255',
         ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Role $role)
+    public function destroy(Role $role ,Project $project)
     {
-        $role->delete();
+        $project->roles()->delete();
         return response()->json(null, 204);
     }
 }
