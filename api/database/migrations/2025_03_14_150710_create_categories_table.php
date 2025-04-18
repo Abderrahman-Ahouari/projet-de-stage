555dcf2e220c349c7758ddf2ea653fb084c284af
yassine snaiki <yassnaiki@gmail.com>
2025-04-18 09:14:40 +0100

    back end : linking category to project in migration

--- api/database/migrations/2025_03_14_150710_create_categories_table.php
+++ api/database/migrations/2025_03_14_150710_create_categories_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->uuid('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
             $table->timestamps();
         });
         if (!DB::table('categories')->where('name', 'basic')->exists()) {
