18014970e1dd3abbdbd1ce3df802188224570134
yassine snaiki <yassnaiki@gmail.com>
2025-04-21 14:05:33 +0100

    backend : sorting queried projects by date

--- api/app/Http/Controllers/ProjectController.php
+++ api/app/Http/Controllers/ProjectController.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
 
-use App\Models\Contribution;
+namespace App\Http\Controllers;
 use App\Models\Project;
-use App\Models\Role;
 use Illuminate\Http\Request;
+
 
 class ProjectController extends Controller
 {
@@ -14,9 +13,10 @@ class ProjectController extends Controller
         return response()->json($admin);
     }
 
+
     public function index()
     {
-        $projects = Project::with('contributors')->paginate(10);
+        $projects = Project::with('contributors')->orderBy('created_at', 'desc')->get();
         if ($projects->isEmpty()) {
             return response()->json([
                 'message' => 'No projects found'
@@ -27,6 +27,8 @@ class ProjectController extends Controller
         return response()->json($projects);
     }
 
+
+
     public function show(Project $project){
         $project->load(['contributors', 'tasks', 'categories.tasks', 'roles']);
     foreach ($project->contributors as $contributor) {
@@ -35,20 +37,21 @@ class ProjectController extends Controller
         return response()->json($project);
     }
 
+
+
     public function userProjects(Request $request){
-        $projects = $request->user()->ownProjects;
+        $projects = $request->user()->ownProjects()->orderBy('created_at', 'desc')->get();
         $projects->load('contributors','tasks');
         return response()->json($projects);
     }
     public function sharedProjects(Request $request){
-        $projects = $request->user()->sharedProjects;
+        $projects = $request->user()->sharedProjects()->orderBy('created_at', 'desc')->get();
         $projects->load('contributors','tasks');
         return response()->json($projects);
     }
 
     public function store(Request $request)
     {
-        // return response()->json($request->all());
         $data = $request->validate([
             'title' => 'required|string|max:20',
             'description' => 'string|max:255',
