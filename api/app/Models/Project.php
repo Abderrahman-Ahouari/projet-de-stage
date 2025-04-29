ad9485009c13e55552ee1feb7a927482663cd6ca
yassine snaiki <yassnaiki@gmail.com>
2025-04-29 14:23:14 +0100

    backend  updating project model relations methods

--- api/app/Models/Project.php
+++ api/app/Models/Project.php
@@ -15,21 +15,30 @@ class Project extends Model
     public function tasks(){
         return $this->hasMany(Task::class);
     }
-    public function contributors(){
-        return $this->belongsToMany(User::class,'contributions')
-        ->withPivot('id','role_id')
-        ->wherePivot('role_id','!=',1);
+    public function contributors()
+    {
+        // Get role IDs that are not "admin" for this project
+        $adminRoleId = $this->roles()->where('name', 'admin')->value('id');
+
+        return $this->belongsToMany(User::class, 'contributions')
+            ->withPivot('role_id')
+            ->wherePivot('role_id', '!=', $adminRoleId);
+    }
+
+    public function admin()
+    {
+        // Get the ID of the admin role for this project
+        $adminRoleId = $this->roles()->where('name', 'admin')->value('id');
+
+        return $this->belongsToMany(User::class, 'contributions')
+            ->withPivot('role_id')
+            ->wherePivot('role_id', $adminRoleId);
     }
     public function categories(){
         return $this->hasMany(Category::class);
     }
     public function roles(){
         return $this->hasMany(Role::class);
-    }
-    public function admin(){
-        return $this->belongsToMany(User::class,'contributions')
-        ->withPivot('role_id')
-        ->wherePivot('role_id',1);
     }
     protected $fillable = [
         'title',
@@ -42,6 +51,54 @@ class Project extends Model
             'id' => 'string',
             'password' => 'hashed',
         ];
+    }
+    protected static function booted()
+    {
+        static::created(function ($project) {
+            $defaultRoles = ['admin', 'read', 'write'];
+
+            // Fetch all permissions once to avoid multiple queries
+            $permissions = permission::all()->keyBy('name');
+
+            foreach ($defaultRoles as $roleName) {
+                $role = $project->roles()->create([
+                    'name' => $roleName,
+                ]);
+
+                if ($roleName === 'admin') {
+                    // Assign all permissions to admin
+                    $role->permissions()->sync($permissions->pluck('id'));
+                }
+
+                if ($roleName === 'read') {
+                    // Assign only read permissions
+                    $role->permissions()->sync([
+                        $permissions['consult project']->id,
+                        $permissions['consult progress']->id,
+                        $permissions['consult team']->id,
+                    ]);
+                }
+
+                if ($roleName === 'write') {
+                    // All except: delete project, manage team, manage roles
+                    $writePermissions = $permissions->except([
+                        'delete project',
+                        'manage team',
+                        'manage roles',
+                    ]);
+                    $role->permissions()->sync($writePermissions->pluck('id'));
+                }
+            }
+        });
+        static::created(function ($project) {
+            $defaultCategories = ['basic', 'feature', 'bug'];
+
+            foreach ($defaultCategories as $category) {
+                $project->categories()->create([
+                    'name' => $category,
+                ]);
+            }
+        });
     }
 
 }
